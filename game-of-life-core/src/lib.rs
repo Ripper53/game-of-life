dffd94bac28360c116b7fa029a4b6a7109403aa1
Albar <GalacticHyperStar@gmail.com>
2024-04-24 15:06:16 -0400

    Tests for next method.

--- game-of-life-core/src/lib.rs
+++ game-of-life-core/src/lib.rs
@@ -41,6 +41,7 @@ impl<T: Grid> GameOfLife<T> {
                 }
             }
         }
+        self.grid.update();
     }
     pub fn grid(&self) -> &T {
         &self.grid
@@ -60,12 +61,95 @@ mod tests {
     pub const HEIGHT: usize = 12;
     pub type TestGrid = GameOfLifeGrid<WIDTH, HEIGHT>;
 
-    #[test]
-    fn next_test() {
-        let mut grid = GameOfLife::<TestGrid>::default();
-        grid.next();
-    }
     proptest! {
+        /// Tests death of cell via underpopulation,
+        /// because it is alone.
+        #[test]
+        fn next_alone_death_test(x in 0..WIDTH, y in 0..HEIGHT) {
+            let mut game = GameOfLife::<TestGrid>::default();
+            game.grid_mut().set(x, y, Cell::Alive);
+            game.grid_mut().update();
+            let cell = game.grid().get(x, y);
+            assert!(cell.is_ok());
+            assert_eq!(Cell::Alive, cell.unwrap());
+            game.next();
+            let cell = game.grid().get(x, y);
+            assert!(cell.is_ok());
+            assert_eq!(Cell::Dead, cell.unwrap());
+        }
+        /// Tests death of cell via underpopulation,
+        /// but not alone.
+        #[test]
+        fn next_underpopulation_death_test(x in 0..WIDTH-1, y in 0..HEIGHT) {
+            let mut game = GameOfLife::<TestGrid>::default();
+            game.grid_mut().set(x, y, Cell::Alive);
+            game.grid_mut().set(x + 1, y, Cell::Alive);
+            game.grid_mut().update();
+            let cell = game.grid().get(x, y);
+            assert!(cell.is_ok());
+            assert_eq!(Cell::Alive, cell.unwrap());
+            let cell = game.grid().get(x + 1, y);
+            assert!(cell.is_ok());
+            assert_eq!(Cell::Alive, cell.unwrap());
+            game.next();
+            let cell = game.grid().get(x, y);
+            assert!(cell.is_ok());
+            assert_eq!(Cell::Dead, cell.unwrap());
+            let cell = game.grid().get(x + 1, y);
+            assert!(cell.is_ok());
+            assert_eq!(Cell::Dead, cell.unwrap());
+        }
+        /// Tests death of cell via overpopulation.
+        #[test]
+        fn next_overpopulation_death_test(x in 1..WIDTH-1, y in 0..HEIGHT-1) {
+            let mut game = GameOfLife::<TestGrid>::default();
+            game.grid_mut().set(x, y, Cell::Alive);
+            game.grid_mut().set(x + 1, y, Cell::Alive);
+            game.grid_mut().set(x + 1, y + 1, Cell::Alive);
+            game.grid_mut().set(x, y + 1, Cell::Alive);
+            game.grid_mut().set(x - 1, y + 1, Cell::Alive);
+            game.grid_mut().update();
+            let cell = game.grid().get(x, y);
+            assert!(cell.is_ok());
+            assert_eq!(Cell::Alive, cell.unwrap());
+            game.next();
+            let cell = game.grid().get(x, y);
+            assert!(cell.is_ok());
+            assert_eq!(Cell::Dead, cell.unwrap());
+        }
+        /// Tests production of a cell, and the staying alive of cells.
+        #[test]
+        fn next_produce_square_pattern_test(x in 1..WIDTH-1, y in 1..HEIGHT-1) {
+            let mut game = GameOfLife::<TestGrid>::default();
+            // Keep cell at (1, 1) alive
+            game.grid_mut().set(x, y, Cell::Alive);
+            game.grid_mut().set(x + 1, y, Cell::Alive);
+            game.grid_mut().set(x, y + 1, Cell::Alive);
+            game.grid_mut().update();
+            // Run behavior a few times
+            // state should not change after
+            // first call of `next`
+            for _ in 0..4 {
+                // Next should keep all cells alive,
+                // AND make a fourth one at (2, 2)
+                game.next();
+                // User Activated Cells
+                let cell = game.grid().get(x, y);
+                assert!(cell.is_ok());
+                assert_eq!(Cell::Alive, cell.unwrap());
+                let cell = game.grid().get(x + 1, y);
+                assert!(cell.is_ok());
+                assert_eq!(Cell::Alive, cell.unwrap());
+                let cell = game.grid().get(x, y + 1);
+                assert!(cell.is_ok());
+                assert_eq!(Cell::Alive, cell.unwrap());
+                // Fourth Automatic Activated Cell
+                // because we called `next`
+                let cell = game.grid().get(x + 1, y + 1);
+                assert!(cell.is_ok());
+                assert_eq!(Cell::Alive, cell.unwrap());
+            }
+        }
         #[test]
         fn keep_grid_test(x in 0..WIDTH, y in 0..HEIGHT) {
             let mut grid = GameOfLife::<TestGrid>::default();
